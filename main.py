3ca4213440337f437f8ea29bc91e196729e28e5d
raymond zhao <raymnzd@gmail.com>
2019-08-29 20:51:27 -0400

    threading problem, need to learn pyqt5

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pyttsx3
-import texttospeech 
+import texttospeech
+import time
 from PyQt5.QtWidgets import QApplication, QLabel
 
 def main():
     app = QApplication([])
     label = QLabel("Spell the word")
     label.show()
+    tts = texttospeech.TTS()
     app.exec_()
 
 if __name__ == "__main__":
