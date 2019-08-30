833d73f19a3d1c120f2602f9ba7ec385f4c42ac2
raymond zhao <raymnzd@gmail.com>
2019-08-29 20:51:03 -0400

    word list instantiated

--- texttospeech.py
+++ texttospeech.py
@@ -5,6 +5,10 @@ class TTS:
     def __init__(self):
         self.engine = pyttsx3.init()
         self.engine.setProperty('voice','com.apple.speech.synthesis.voice.daniel')
+        self.word_list = [word for word in open('words.txt','r')]  
+        print(self.word_list)
+    def get_word(self):
+        pass
 
     def say_word(self):
         self.engine.say("hello")
